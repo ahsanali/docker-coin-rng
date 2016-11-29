566c21ff4f185d0f989fef8c45130cb3d45fcc0e
Muhammad Ahsan Ali <ahsan@flux7.com>
2016-11-29 17:47:35 +0500

    testing out with BB

--- rng/rng.py
+++ rng/rng.py
@@ -6,7 +6,7 @@ import time
 app = Flask(__name__)
 
 # Enable debugging if the DEBUG environment variable is set and starts with Y
-# Demo 
+# Demo BB
 app.debug = os.environ.get("DEBUG", "").lower().startswith('y')
 
 hostname = socket.gethostname()
